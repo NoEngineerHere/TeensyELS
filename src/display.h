b876fc66e98fe698af26e3f7973d5fb052dfeb31
Jack Hosemans <jh247247@gmail.com>
2024-07-10 16:17:47 +1000

    Add config and display files

---
+++ src/display.h
@@ -0,0 +1,40 @@
+
+#include "config.h"
+
+#define SSD1306_128_64 0
+
+#if ELS_DISPLAY == SSD1306_128_64
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define SCREEN_ADDRESS 0x3C  // this is the default address of the SSD1306
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#else
+
+#error "Please choose a valid display. Refer to config.h for options"
+
+#endif
+
+class Display {
+ public:
+#if ELS_DISPLAY == SSD1306_128_64
+  Adafruit_SSD1306 m_ssd1306;
+#endif
+  Display() {
+#if ELS_DISPLAY == SSD1306_128_64
+    this->m_ssd1306 =
+        Adafruit_SSD1306(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, PIN_DISPLAY_RESET);
+#endif
+  }
+
+  void init();
+  void update(boolean driveMode, float pitch, boolean lock, boolean enabled);
+
+ protected:
+  void drawMode(boolean driveMode);
+  void drawPitch(float rate);
+  void drawEnabled(boolean enabled);
+  void drawLocked(boolean locked);
+};
